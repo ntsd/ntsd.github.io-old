370885004e25dbf35e7a2cfd04be9ec9c00b57e9
ntsd <jo06942@gmail.com>
2020-04-11 10:27:22 +0700

    Add font awesome to service worker

--- sw.js
+++ sw.js
@@ -25,9 +25,8 @@ const PRECACHE_LIST = [
   "./css/ntsd-blog.min.css",
   "./css/syntax.css",
   "./css/bootstrap.min.css",
-  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
-  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
-  // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
+  "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js",
+  "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
