510a3a58c00231104eff3e3917bc7ab99af30645
ntsd <jo06942@gmail.com>
2020-09-16 02:45:07 +0700

    feat: update portfolio delay

--- portfolio/js/timeline.js
+++ portfolio/js/timeline.js
@@ -1,147 +1,144 @@
-(function($) {
+(function ($) {
+  $(document).ready(function () {
+    setupFade();
+    setupClickToScroll();
+    setupPostAnimation();
+    setupScrollToTop();
+    enableScrollAbortion();
 
-	$(document).ready(function() {
-		setupFade();
-		setupClickToScroll();
-		setupPostAnimation();
-		setupScrollToTop();
-     enableScrollAbortion();
-
-		$(window).scroll();
+    $(window).scroll();
   });
-  
+
   function enableScrollAbortion() {
-    var $viewport = $('html, body');
-    $viewport.on('scroll mousedown DOMMouseScroll mousewheel keyup', function(e) {
-        if ( e.which > 0 || e.type === 'mousedown' || e.type === 'mousewheel') {
-             $viewport.stop();
-        }
+    var $viewport = $("html, body");
+    $viewport.on("scroll mousedown DOMMouseScroll mousewheel keyup", function (
+      e
+    ) {
+      if (e.which > 0 || e.type === "mousedown" || e.type === "mousewheel") {
+        $viewport.stop();
+      }
     });
   }
 
-	function setupScrollToTop() {
-		var scrollSpeed = 750;
-		$('.trigger-scroll-to-top').click(function(e) {
-			e.preventDefault();
+  function setupScrollToTop() {
+    var scrollSpeed = 750;
+    $(".trigger-scroll-to-top").click(function (e) {
+      e.preventDefault();
 
-			var post = $('#timeline .post-wrapper .timeline-post').first(),
-				postTopOffset = post.offset().top,
-				postHeight = post.height(),
-				halfScreen = $(window).height() / 2,
-				scrollTo = postTopOffset - halfScreen + (postHeight/2);
+      var post = $("#timeline .post-wrapper .timeline-post").first(),
+        postTopOffset = post.offset().top,
+        postHeight = post.height(),
+        halfScreen = $(window).height() / 2,
+        scrollTo = postTopOffset - halfScreen + postHeight / 2;
 
-			$('html, body').animate({
-				scrollTop: scrollTo
-			}, scrollSpeed);
-		});
-	}
+      $("html, body").animate(
+        {
+          scrollTop: scrollTo,
+        },
+        scrollSpeed
+      );
+    });
+  }
 
+  function setupPostAnimation() {
+    var posts = $("#timeline .post-wrapper .timeline-post");
+    $(window).on("scroll resize", function () {
+      var currScroll =
+          $(window).scrollTop() > $(document).scrollTop()
+            ? $(window).scrollTop()
+            : $(document).scrollTop(),
+        windowHeight = $(window).height(),
+        overScroll = Math.ceil(windowHeight * 0.2),
+        treshhold = currScroll + windowHeight - overScroll;
 
-	function setupPostAnimation() {
-		var posts = $('#timeline .post-wrapper .timeline-post');
-		$(window).on('scroll resize', function() {
+      posts.each(function () {
+        var post = $(this),
+          postScroll = post.offset().top;
 
-			var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
-				windowHeight = $(window).height(),
-				overScroll = Math.ceil(windowHeight*.20),
-				treshhold = (currScroll + windowHeight) - overScroll;
+        if (postScroll > treshhold) {
+          post.addClass("post-hidden");
+        } else {
+          post.removeClass("post-hidden");
+        }
+      });
+    });
+  }
 
-			posts.each(function() {
+  function setupFade() {
+    var posts = $("#timeline .post-wrapper .timeline-post").reverse(),
+      stemWrapper = $("#timeline .stem-wrapper"),
+      halfScreen = $(window).height() / 2;
 
-				var post = $(this),
-					postScroll = post.offset().top;
+    $(window).on("scroll resize", function () {
+      delay(function () {
+        var currScroll =
+            $(window).scrollTop() > $(document).scrollTop()
+              ? $(window).scrollTop()
+              : $(document).scrollTop(),
+          scrollSplit = currScroll + halfScreen;
 
-				if(postScroll > treshhold) {
-					post.addClass('post-hidden');
-				} else {
-					post.removeClass('post-hidden');
-				}
+        posts.removeClass("post-active").each(function () {
+          var post = $(this),
+            postOffset = post.offset().top;
 
-			});
+          if (scrollSplit > postOffset) {
+            post.addClass("post-active");
 
-		});
-	}
+            var color = post.data("stem-color")
+                ? post.data("stem-color")
+                : null,
+              allColors =
+                "color-green color-yellow color-white color-purple color-pink color-red color-blue color-grey color-black";
 
-	function setupFade() {
+            stemWrapper.removeClass(allColors);
 
-		var posts = $('#timeline .post-wrapper .timeline-post').reverse(),
-			stemWrapper = $('#timeline .stem-wrapper'),
-			halfScreen = $(window).height() / 2;
+            if (color !== null) {
+              stemWrapper.addClass("color-" + color);
+            }
 
-		$(window).on('scroll resize', function() {
+            return false;
+          }
+        });
+      }, 20);
+    });
+  }
 
-			delay(function() {
+  function setupClickToScroll(post) {
+    var scrollSpeed = 750;
 
-				var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
-					scrollSplit = currScroll + halfScreen;
+    $("#timeline .post-wrapper .timeline-post .stem-overlay .icon").click(
+      function (e) {
+        e.preventDefault();
 
-				posts.removeClass('post-active').each(function() {
+        var icon = $(this),
+          post = icon.closest(".timeline-post"),
+          postTopOffset = post.offset().top,
+          postHeight = post.height(),
+          halfScreen = $(window).height() / 2,
+          scrollTo = postTopOffset - halfScreen + postHeight / 2;
 
-					var post = $(this),
-						postOffset = post.offset().top;
-
-					if(scrollSplit > postOffset) {
-
-						post.addClass('post-active')
-
-						var color = post.data('stem-color') ? post.data('stem-color') : null,
-							allColors = 'color-green color-yellow color-white color-purple color-pink color-red color-blue color-grey color-black'
-
-						stemWrapper.removeClass(allColors);
-
-						if(color !== null) {
-							stemWrapper.addClass('color-' + color);
-						}
-
-						return false;
-					}
-
-				});
-			}, 20);
-
-		});
-
-	}
-
-
-	function setupClickToScroll(post) {
-
-		var scrollSpeed = 750;
-
-		$('#timeline .post-wrapper .timeline-post .stem-overlay .icon').click(function(e) {
-			e.preventDefault();
-
-			var icon = $(this),
-				post = icon.closest('.timeline-post'),
-				postTopOffset = post.offset().top,
-				postHeight = post.height(),
-				halfScreen = $(window).height() / 2,
-				scrollTo = postTopOffset - halfScreen + (postHeight/2);
-
-			$('html, body').animate({
-				scrollTop: scrollTo
-			}, scrollSpeed);
-		});
-
-	}
-
+        $("html, body").animate(
+          {
+            scrollTop: scrollTo,
+          },
+          scrollSpeed
+        );
+      }
+    );
+  }
 })(jQuery);
-
-
-
 
 /*==========  Helpers  ==========*/
 
-
 // Timeout function
-var delay = (function(){
-	var timer = 0;
-	return function(callback, ms){
-		clearTimeout (timer);
-		timer = setTimeout(callback, ms);
-	};
+var delay = (function () {
+  var timer = 0;
+  return function (callback, ms) {
+    clearTimeout(timer);
+    timer = setTimeout(callback, ms);
+  };
 })();
 
-$.fn.reverse = function() {
-    return this.pushStack(this.get().reverse(), arguments);
+$.fn.reverse = function () {
+  return this.pushStack(this.get().reverse(), arguments);
 };
